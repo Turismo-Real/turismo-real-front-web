cfb892553d402a736fd1ee97c4cf099c63084d00
eNorese <enzo.norese@gmail.com>
2021-11-23 01:40:27 -0300

    Llamada API login

--- turismo_real_app/services/login.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-URL_LOGIN = 'http://localhost:5000/api/v1/login'
-
-def loginRequest(email, password):
-    payload = { email, password }
-    response = requests.post(URL_LOGIN, json=payload)
-    json = response.json()
-    return json
--- turismo_real_app/views.py
+++ turismo_real_app/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.http.response import HttpResponseGone
 from django.shortcuts import render
-
+import requests
 
 def index(request):
     return render(request, 'index.html')
@@ -24,16 +24,24 @@ def funcionario(request):
 async def login(request):
     correo = request.POST['correo']
     passwd = request.POST['hashedPassword']
-    # response = loginRequest(correo, passwd)
-    
-    print(correo)
-    print(passwd)
+    response = login_request(correo, passwd)
 
-    # if response['login']:
-    #     tipo = response['tipo'].upper()
-    #     if tipo == 'CLIENTE':
-    #         return render(request, 'client.html')
-    #     elif tipo == 'FUNCIONARIO':
-    #         return render(request, 'funcionario.html')
-    # return render(request, 'index.html')
+    if response['login']:
+        tipo = response['tipo'].upper()
+        if tipo == 'CLIENTE':
+            return render(request, 'client.html')
+        elif tipo == 'FUNCIONARIO':
+            return render(request, 'funcionario.html')
     return render(request, 'index.html')
+
+# LLAMADAS A APIs
+def login_request(email, password):
+    URL_LOGIN = 'http://localhost:5000/api/v1/login'
+    _headers = { 'Content-Type': 'application/json' }
+    payload = {
+        'email': email, 
+        'password': password 
+    }
+    response = requests.post(URL_LOGIN, json=payload, headers=_headers)
+    json = response.json()
+    return json
